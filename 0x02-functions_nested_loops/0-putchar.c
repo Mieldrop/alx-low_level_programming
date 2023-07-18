a0cb4944fd062895d58bd47c24054fb1c7a41fa2
Mieldrop <arikeod15@gmail.com>
2023-07-18 14:23:35 -0700

    0 putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("_putchar \n");
+	return (0);
+}
