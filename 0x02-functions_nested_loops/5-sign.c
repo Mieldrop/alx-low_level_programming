3140b5bffbb0a95ab8f968b917840c90f8c88fe1
Mieldrop <arikeod15@gmail.com>
2023-07-18 16:44:13 -0700

    5 sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,9 @@
 
 /**
  * print_sign - Entry point
- * Return: 0
+ * Return: 1 and print + if is greater than zero
+ * 0 and ptint 0 if is zero
+ * -1 and print - if is less than 0
  */
 
 int print_sign(int n)
@@ -12,14 +14,12 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-
 	else if (n == 0)
 	{
-		_putchar(0);
+		_putchar(48);
 		return (0);
 	}
-
-	else
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
