0e6456765de4e6a546f731390b362d01ef1b2448
Mieldrop <arikeod15@gmail.com>
2023-07-20 18:19:01 -0700

    function 5

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * print_numbers - print numbers range 0 - 9
- *
- * Return: nothing
+ * print_numbers - print numbers ranging from 0 to 9
+ * Return: Nothing
  */
+
 void print_numbers(void)
 {
 	int a;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 48; a <= 57; a++)
 	{
 		_putchar(a);
 	}
