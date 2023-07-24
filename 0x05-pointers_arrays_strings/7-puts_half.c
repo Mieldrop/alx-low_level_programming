bcf9d5f00b692b93b2982be378b87bd49574cb43
Mieldrop <arikeod15@gmail.com>
2023-07-24 15:19:04 -0700

    string 6

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: print the second half ofthe string
+ * if the number of character  is odd,
+ * it should print the last n chatacter of the string '
+ * where n = (length_of_the_string - 1) / 2
+ * Return: n
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 1)
+	{
+		j = (i - 1) / 2;
+		j += 1;
+	}
+	else
+	{
+		j = i / 2;
+	}
+
+	for (; j < i; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
