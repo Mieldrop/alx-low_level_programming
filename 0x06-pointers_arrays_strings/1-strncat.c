3eb2f2842ac6cb2c64559bdaf2f6e15f9597c826
Mieldrop <arikeod15@gmail.com>
2023-07-26 11:20:32 -0700

    first update

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: first string
+ * @src: second string
+ * @n: input integer
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
