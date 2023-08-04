84689084a587294cf00d6398b2c08a3df2cd55b0
Mieldrop <arikeod15@gmail.com>
2023-08-04 06:19:26 -0700

    libmy.a file

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strspn - gets the length of prefix substring
+ * @s: initial segment
+ * @accept: byte
+ * Return: Nothing
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+		{
+			n++;
+			break;
+		}
+		else if (accept[i + 1] == '\0')
+			return (n);
+		}
+		s++;
+	}
+	return (n);
+}
---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strstr - locates a substring
+ * @haystack: the string
+ * @needle: the substring
+ * Return: pointer to the beginning of the located string,
+ * or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *i = haystack;
+		char *j = needle;
+
+		while (*i == *j && *j != '\0')
+		{
+			i++;
+			j++;
+		}
+			if (*j == '\0')
+				return (haystack);
+	}
+	return (0);
+}
