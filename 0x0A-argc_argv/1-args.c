d35937968c41d4142d88d1fba90f63e311cba9da
Mieldrop <arikeod15@gmail.com>
2023-08-04 07:30:53 -0700

    task 2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argument
+ * @argv: argument array
+ * Return: Nothing
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
