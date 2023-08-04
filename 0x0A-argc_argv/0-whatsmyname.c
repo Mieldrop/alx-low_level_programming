d637557429f791724c958fd64de1ab5dbc37bef1
Mieldrop <arikeod15@gmail.com>
2023-08-04 07:20:35 -0700

    task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: number of arrays
+ * Return: Nothing
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
