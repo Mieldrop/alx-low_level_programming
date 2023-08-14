7b08a5e7e5e21da635e558f8f6b1aca3e5fabe8d
Mieldrop <arikeod15@gmail.com>
2023-08-14 06:32:25 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * STRUCT dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ * Description: just a lone dog struct in a kitty world
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
