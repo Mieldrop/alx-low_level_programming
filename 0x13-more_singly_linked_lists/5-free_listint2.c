a0d425aabbc038ecf89daf61f32eadb376834da9
Mieldrop <arikeod15@gmail.com>
2023-08-29 12:11:46 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a list
+ * @head: the listint_t list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node, *tmp;
+
+	if (!head)
+		return;
+
+	node = *head;
+	while (node)
+	{
+		tmp = node;
+		node = node->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
