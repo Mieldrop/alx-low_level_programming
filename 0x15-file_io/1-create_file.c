fb7a9cc1c9907f5a6aa0055a67feb8121663933f
Mieldrop <arikeod15@gmail.com>
2023-09-04 07:17:52 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @n: the string
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * create_file - creates files
+ * @filename: name the file created
+ * @text_content: the text contained in the file created
+ * Return: 1 on success, 0 if it fails
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t byte = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file == -1)
+		return (-1);
+	if (len)
+		byte = write(file, text_content, len);
+	close(file);
+	return (byte == len ? 1 : -1);
+}
